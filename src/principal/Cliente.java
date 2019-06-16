a9492befd9d92cf94a8a76580a6eadd5a8876360
fponce06 <fponce@espol.edu.ec>
2019-06-16 00:57:15 -0500

    Se implemento la clase Cliente con sus respectivos getters and setters y su constructor.

---
+++ src/principal/Cliente.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Freddy
+ */
+public class Cliente {
+    private String identificacion;
+    private String ciudadResi;
+    private String nombre;
+    private String email;
+    private ArrayList<String> numerosContacto;
+    private String numeroTarjeta;
+
+    public Cliente(String identificacion, String ciudadResi, String nombre, String email, ArrayList<String> numerosContacto, String numeroTarjeta) {
+        this.identificacion = identificacion;
+        this.ciudadResi = ciudadResi;
+        this.nombre = nombre;
+        this.email = email;
+        this.numerosContacto = numerosContacto;
+        this.numeroTarjeta = numeroTarjeta;
+    }
+    
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public String getCiudadResi() {
+        return ciudadResi;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public ArrayList<String> getNumerosContacto() {
+        return numerosContacto;
+    }
+
+    public String getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public void setCiudadResi(String ciudadResi) {
+        this.ciudadResi = ciudadResi;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNumerosContacto(ArrayList<String> numerosContacto) {
+        this.numerosContacto = numerosContacto;
+    }
+
+    public void setNumeroTarjeta(String numeroTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+    }
+    
+}
