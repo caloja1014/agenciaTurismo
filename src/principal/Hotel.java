7554e6ad0736648ce92377ad653bb499f9e58e8d
Gaby Pazmino <gabypazmino99@gmail.com>
2019-06-16 00:02:52 -0500

    Se crea la clase hotel. Esta clase se relacionara con la clase agencia y tiene relación con ciudad, provincia y clase habitación.

---
+++ src/principal/Hotel.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author gabrielapazmino
+ */
+public class Hotel {
+    private String idHotel;
+    private String idCiudad;
+    private String nombre;
+    private String descipcionHotel;
+    private String targetaHotel;
+    private String ubicacion;
+    private String direccion;
+    private String webHotel;
+    private String clasificacion;
+    private String foto;
+    private String latitud;
+    private String longitud;
+    
+        //setters
+        public void setidHotel(String idHotel){
+            this.idHotel=idHotel;
+            }
+        public void setidCiudad(String idCiudad){
+            this.idCiudad=idCiudad;
+            }
+        public void setnombre(String nombre){
+            this.nombre=nombre;
+            }
+        public void setdescipcionHotel(String descipcionHotel){
+            this.descipcionHotel=descipcionHotel;
+            }
+        public void settargetaHotel(String targetaHotel){
+            this.targetaHotel=targetaHotel;
+            }
+        public void setubicacion(String ubicacion){
+            this.ubicacion=ubicacion;
+            }
+        public void setdireccion(String direccion){
+            this.direccion=direccion;
+            }
+        public void setwebHotel(String webHotel){
+            this.webHotel=webHotel;
+            }
+        public void setclasificacion(String clasificacion){
+            this.clasificacion=clasificacion;
+            }
+        public void setfoto(String foto){
+            this.foto=foto;
+            }
+        public void setlatitud(String latitud){
+            this.latitud=latitud;
+            }
+        public void setlongitud(String longitud){
+            this.longitud=longitud;
+            }
+
+        //getters
+        public String idHotel(){
+            return idHotel;
+             }        
+        public String idCiudad(){
+            return idCiudad;
+         } 
+        public String nombre(){
+            return nombre;
+            }        
+        public String descipcionHotel(){
+            return descipcionHotel;
+            } 
+        public String targetaHotel(){
+            return targetaHotel;
+            }        
+        public String ubicacionl(){
+            return ubicacion;
+            }
+        public String direccion(){
+            return direccion;
+             }        
+        public String webHotel(){
+            return webHotel;
+         } 
+        public String clasificacion(){
+            return clasificacion;
+            }        
+        public String foto(){
+            return foto;
+            } 
+        public String latitud(){
+            return latitud;
+            }        
+        public String longitud(){
+            return longitud;
+            }     
+          
+}
