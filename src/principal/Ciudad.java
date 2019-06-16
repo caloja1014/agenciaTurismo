9fcca4f15d2de00ceddd72f8fc19b67f48645a37
Your Name <caloja@espol.edu.ec>
2019-06-15 23:12:31 -0500

    Se crea la clase ciudad para relacionarla con la clase agencia.

---
+++ src/principal/Ciudad.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author Cloja
+ */
+public class Ciudad {
+    private String idCiudad;
+    private String idProvincia;
+    private String nombre;
+
+    /**
+     * @return the idCiudad
+     */
+    public String getIdCiudad() {
+        return idCiudad;
+    }
+
+    /**
+     * @return the idProvincia
+     */
+    public String getIdProvincia() {
+        return idProvincia;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+    
+    
+}
