ce9fa931fcad27e4c6692493fa462e6811333614
Carlos Andres Loja Guevara <caloja@espol.edu.ec>
2019-07-08 00:55:13 -0500

    Arreglo del metodo MostrarInformacion

--- src/Menu/MenuCliente.java
+++ src/Menu/MenuCliente.java
@@ -197,23 +197,29 @@ public class MenuCliente {
         return client;
     }
     
-    public ArrayList<Hotel> mostrarInformacion(String ciudad,String rango,ArrayList<Habitacion> habitaciones,ArrayList<Ciudad> ciudades){
+   public ArrayList<Hotel> mostrarInformacion(String ciudad,String rango,ArrayList<Habitacion> habitaciones,ArrayList<Ciudad> ciudades){
         String[] rangoPrecio=rango.split("a");
         Double rangoSuperior=Double.parseDouble(rangoPrecio[0].trim());
         Double rangoInferior=Double.parseDouble(rangoPrecio[1].trim());
         Scanner s= new Scanner(System.in);
         ArrayList<Habitacion> habitacionesEscogidas=new ArrayList();
         ArrayList<Hotel> hoteles= new ArrayList();
+        
         for (Habitacion habitacion: habitaciones ){
-            int indice=0;
-             if (habitacion.getTarifaSencilla()>=rangoInferior && (habitacion.getTarifaDoble()<=rangoSuperior || habitacion.getTarifaTriple()<=rangoSuperior) && ciudades.get(indice).getNombre().equalsIgnoreCase(ciudad) )
-                indice++;            
+             if (habitacion.getTarifaSencilla()>=rangoInferior && (habitacion.getTarifaDoble()<=rangoSuperior || habitacion.getTarifaDoble()==0.0||habitacion.getTarifaTriple()<=rangoSuperior||habitacion.getTarifaTriple()==0.00 )){
                 int indiceHotel=Integer.parseInt(habitacion.getIdHotel())-1;
                 Hotel hotel=data.Data.hoteles.get(indiceHotel);
+                
+                String ciu= ciudades.get(Integer.parseInt(hotel.getIdCiudad())-1).getNombre();
+                if (ciu.equalsIgnoreCase(ciudad)){
+                     
+
                 if (!hoteles.contains(hotel)){
                     hoteles.add(hotel);
                     habitacionesEscogidas.add(habitacion);
                 }
+                }
+            }
         }
         if (hoteles.size()>0){
             return hoteles;
@@ -221,8 +227,7 @@ public class MenuCliente {
         else{
             return null;
         }
-        
-    }
+   }
     
     public ArrayList<Hotel> mostrarInformacion(String ciudad,String rango,ArrayList<Habitacion> habitaciones,String servicio,ArrayList<Ciudad> ciudades){
         ArrayList<Hotel>hotelesEscogidos=this.mostrarInformacion(ciudad,rango, habitaciones,ciudades);
