f6f144505a850c02cdc7d3a199ecee50c502bc65
Gaby Pazmino <gabypazmino99@gmail.com>
2019-07-07 20:08:18 -0500

    Creacion de metodos necesarios para el MenuCliente

--- src/Menu/MenuCliente.java
+++ src/Menu/MenuCliente.java
@@ -5,17 +5,24 @@
  */
 package Menu;
 
+import static Menu.MenuCliente.sc;
 import java.util.ArrayList;
 import java.util.Scanner;
+import principal.Agencia;
 import principal.Ciudad;
 import principal.Habitacion;
 import principal.Hotel;
+import principal.Reserva;
 
 /**
  *
  * @author Freddy
  */
 public class MenuCliente {
+        private static final ArrayList <String> listaValsino=new ArrayList();
+    private static final ArrayList <String> listaCod=new ArrayList();
+    static Scanner sc =new Scanner(System.in);
+    
     public ArrayList<Hotel> mostrarInformacion(String ciudad,String rango,ArrayList<Habitacion> habitaciones,ArrayList<Ciudad> ciudades){
         String[] rangoPrecio=rango.split("a");
         Double rangoSuperior=Double.parseDouble(rangoPrecio[0].trim());
@@ -62,4 +69,43 @@ public class MenuCliente {
         return null;
         }
     }
+
+    public void eliminarReserva(){
+    
+    boolean opcion=true;
+    System.out.println("Lamento escuchar eso, me podría decir el código de su reserva?");
+    
+    while (opcion){
+    String resp=sc.nextLine();
+    int respConv=Integer.parseInt(resp);
+    Reserva rev=new Reserva();
+    if(rev.validarCodigo(respConv)){
+        for(Reserva r:Agencia.reservas){
+            if(resp.equals(r.getCodigo())){
+                Agencia.reservas.remove(r); 
+            }
+        }
+        System.out.println("Su reserva ha sido cancelada exitosamente");
+        opcion=false;
+    }
+    else{
+        System.out.println("El código ingresado es incorrecto, favor ingrese el código nuevamente");
+    }
+    }
+
 }
+
+    public void realizarNada(){
+
+    System.out.println("No entiendo, desea reservar un hotel? ");
+    String reserva=sc.nextLine();
+            
+    
+        if (reserva.equalsIgnoreCase("si")){
+            //mostrarInformacion();
+             
+        }else if(reserva.equalsIgnoreCase("no")){
+            
+        }
+}}
+
